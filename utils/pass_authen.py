62a085ce7a63379e2adf60d8c072e9d4607f3eb4
Myra <47497128+sevany@users.noreply.github.com>
2021-06-07 18:22:53 +0800

    Create pass_authen.py

---
+++ utils/pass_authen.py
@@ -0,0 +1,8 @@
+##generate random secret key : openssl rand -hex 32
+
+
+JWT_SECRET_KEY = "014acc74cab79220ef7faad3e749773660c4136c381a65039afa849398e741cf"
+
+JWT_ALGORITH = "HS256"
+
+JWT_EXPIRATION_TIMES_MINUTES = 60 * 24 * 5
