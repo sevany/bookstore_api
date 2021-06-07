018d5f7d24ad2c70fa1d3519f22b93257f9d26ad
Myra <47497128+sevany@users.noreply.github.com>
2021-06-07 18:21:48 +0800

    Update version1.py

--- routes/version1.py
+++ routes/version1.py
@@ -1,14 +1,18 @@
+from fastapi import FastAPI, Body, Header, File, Depends, HTTPException
+from models.JWT_user import JWTUser
 from starlette import status
 from starlette import responses
 from models.book import Book
 from models.author import Author
-from fastapi import FastAPI, Body, Header, File
 from models.user import User
-from starlette.status import HTTP_201_CREATED
+from starlette.status import HTTP_201_CREATED, HTTP_401_UNAUTHORIZED
 from starlette.responses import Response
-
+from fastapi.security import OAuth2PasswordRequestForm
+from utils.security import authenticate_user, check_token, create_token
+from models.JWT_user import JWTUser
 
 app_v1 = FastAPI(openapi_prefix="/v1")
+
 
 @app_v1.get("/hello")
 async def hello_world():
@@ -20,10 +24,10 @@ async def validation(password:str):
     return {"query parameter": password}
 
 @app_v1.post("/user", status_code=HTTP_201_CREATED)
-async def user(user:User, x_custom: str = Header(...)):
+async def user(user:User, x_custom:str = Header("default")):
     return {"request body": user, "request_custom_header": x_custom}
 
-@app_v1.get("/book/{isbn}", response_model=Book)
+@app_v1.get("/book/{isbn}", response_model=Book, response_model_include=["name", "year"])
 async def get_book(isbn:str):
     author_dict ={
         "name": "author1",
@@ -61,3 +65,23 @@ async def upload_photo(response:Response, profile_photo: bytes = File(...)):
     return {"file size": len(profile_photo)}
 
 ##endpoint generate token for users
+
+@app_v1.post("/token")
+async def login_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+#check if nama correct ke tak
+
+    jwt_user_dict = {
+        "username": form_data.username,
+        "password": form_data.password
+    }
+    
+    jwt_user = JWTUser(**jwt_user_dict)
+    user = authenticate_user(jwt_user)
+    
+#kalau fail takde nama and pasword kat db kena return unathourized status
+    if user is None:
+        # return HTTP_401_UNAUTHORIZED
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED) #if nak guna http exception
+#kalau ada dalam db boleh create token
+    jwt_token = create_token(user)
+    return{"token": jwt_token}
