06279c3394e044c1139646ac4a16e62d9fec6a62
Myra <47497128+sevany@users.noreply.github.com>
2021-06-11 16:14:36 +0800

    Update pass_authen.py

--- utils/pass_authen.py
+++ utils/pass_authen.py
@@ -6,3 +6,10 @@ JWT_SECRET_KEY = "014acc74cab79220ef7faad3e749773660c4136c381a65039afa849398e741
 JWT_ALGORITH = "HS256"
 
 JWT_EXPIRATION_TIMES_MINUTES = 60 * 24 * 5
+
+ISBN_DESCRIPTION = "it is unique identifier books"
+
+DB_HOST = "localhost"
+DB_USER = "admin"
+DB_PASSWORD = "admin"
+DB_NAME = "bookstore"
